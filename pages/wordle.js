f1d7616f11f37cb5de7649a0961c31697ebf87a1
Rico Apon <rico.apon@gmail.com>
2024-12-01 11:48:23 +0100

    Put text in middle

--- pages/wordle.js
+++ pages/wordle.js
@@ -34,26 +34,26 @@ export default {
         return {page, wordle}
     },
     template: `
-        <div class="text-center" style="max-width: 300px;">
-            
-            <div v-for="guessedWord in wordle.guessedWords" class="d-flex">
-                <div v-for="(guessedLetter, index) in guessedWord">
-                    <component is="letter" :l="guessedLetter" :delay="index"></component>
+        <div class="d-flex justify-content-center pb-2">
+            <div>
+                <div v-for="guessedWord in wordle.guessedWords" class="d-flex text-center">
+                    <div v-for="(guessedLetter, index) in guessedWord">
+                        <component is="letter" :l="guessedLetter" :delay="index"></component>
+                    </div>
                 </div>
             </div>
-            
-            <div id="keyboard-cont">
-                <div class="first-row">
-                    <component is="keyboardKey" v-for="c in 'qwertyuiop'.split('')" :c :wordle></component>
-                </div>
-                <div class="second-row">
-                    <component is="keyboardKey" v-for="c in 'asdfghjkl'.split('')" :c :wordle></component>
-                </div>
-                <div class="third-row">
-                    <component is="keyboardKey" c="Enter" :wordle></component>
-                    <component is="keyboardKey" v-for="c in 'zxcvbnm'.split('')" :c :wordle></component>
-                    <component is="keyboardKey" c="Del" :wordle></component>
-                </div>
+        </div>
+        <div id="keyboard-cont" class="text-center">
+            <div class="pb-1">
+                <component is="keyboardKey" v-for="c in 'qwertyuiop'.split('')" :c :wordle></component>
+            </div>
+            <div class="pb-1">
+                <component is="keyboardKey" v-for="c in 'asdfghjkl'.split('')" :c :wordle></component>
+            </div>
+            <div class="pb-1">
+                <component is="keyboardKey" c="Enter" :wordle></component>
+                <component is="keyboardKey" v-for="c in 'zxcvbnm'.split('')" :c :wordle></component>
+                <component is="keyboardKey" c="Del" :wordle></component>
             </div>
         </div>
     `,
